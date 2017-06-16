7454eaeb480c8ffc1bf2f72d949ed86f48a8e926
jokrey <justin.bennett.developer@gmail.com>
2017-06-16 15:45:04 +0200

    simple refactoring - moved implementations of pipeline to their own package

--- src/evade/game/EvadeGameStarter.java
+++ src/evade/game/EvadeGameStarter.java
@@ -1,9 +1,7 @@
 package evade.game;
 
-import util.animation.java_fx.display.JavaFX_FullScreenStarter;
-import util.animation.java_fx.pipeline.AnimationDrawerJavaFX;
-import util.animation.swing.display.Swing_FullScreenStarter;
-import util.animation.swing.pipeline.AnimationDrawerSwing;
+import util.animation.implementations.java_fx.display.JavaFX_FullScreenStarter;
+import util.animation.implementations.java_fx.pipeline.AnimationDrawerJavaFX;
 
 public class EvadeGameStarter {
 	public static void main(String[] args) {
